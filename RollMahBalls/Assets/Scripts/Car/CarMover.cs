6fde5a5aa043f0b441907d19125f6bb23c0825c8
MuNgLo <kvarnrud@gmail.com>
2019-05-23 21:28:52 +0200

    some input controls added

---
+++ RollMahBalls/Assets/Scripts/Car/CarMover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Car
+{
+    /// <summary>
+    /// This is using leftstick vector from twinstickinput to move the transform it is attached to
+    /// Check the AImer class for rightstick bit
+    /// </summary>
+    [RequireComponent(typeof(CarTwinstickInput))]
+    public class CarMover : MonoBehaviour
+    {
+        //public float _maxAngularSpeed = 90.0f;
+        public float _StreeringSpeed = 10000.0f;
+        public float _Acceleration = 10000.0f;
+        public ForceMode _mode = ForceMode.Force;
+        public bool _useDelta = true;
+
+        public Transform _translator;
+
+        public Rigidbody _Car;
+
+        private CarTwinstickInput tw;
+        private void Start()
+        {
+            tw = GetComponent<CarTwinstickInput>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (!_translator) { return; }
+            if (_useDelta)
+            {
+                _Car.GetComponent<Rigidbody>().AddForce(_translator.TransformVector(Vector3.forward * tw.LVerticalValue * _Acceleration * Time.deltaTime), _mode);
+                _Car.GetComponent<Rigidbody>().AddTorque(
+                    _translator.TransformVector(Quaternion.Euler(0.0f, 0.0f, 90.0f) * Vector3.right * tw.LHorizontalValue * _StreeringSpeed * Time.deltaTime),
+                    ForceMode.Force
+                    );
+
+            }
+            else
+            {
+                _Car.GetComponent<Rigidbody>().AddForce(_translator.TransformVector(Vector3.forward * tw.LVerticalValue * _Acceleration), _mode);
+                _Car.GetComponent<Rigidbody>().AddTorque(
+                    _translator.TransformVector(Quaternion.Euler(0.0f, 0.0f, 90.0f) * Vector3.right  * tw.LHorizontalValue * _StreeringSpeed), 
+                    ForceMode.Force                   
+                    );
+            }
+        }
+
+        public void SetTranslationTransform(Transform tr)
+        {
+            _translator = tr;
+        }
+    }
+}
---
+++ RollMahBalls/Assets/Scripts/Car/CarTwinstickInput.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Car
+{
+    /// <summary>
+    /// Make sure Unity Input has Horizontal2 and Vertical2 for the left stick. Try 4th and 5th Axis first.
+    /// </summary>
+    [RequireComponent(typeof(CarMover))]
+    public class CarTwinstickInput : MonoBehaviour
+    {
+        private float _R_horizontal = 0.0f, _R_vertical = 0.0f;
+        private float _L_horizontal = 0.0f, _L_vertical = 0.0f;
+        public float _deadZone = 0.02f;
+        public bool _useRaw = false;
+        //public AnimationCurve _defaultInputCurve;
+        public Vector3 LeftStickVector { get { return GetLeftStickVector(); } private set { } }
+        public Vector3 RightStickVector { get { return GetRightStickVector(); } private set { } }
+        public float LHorizontalValue { get { return _L_horizontal; } private set { } }
+        public float LVerticalValue { get { return _L_vertical; } private set { } }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_useRaw)
+            {
+                _L_horizontal = ClampDeadZone(Input.GetAxisRaw("Horizontal"));
+                _L_vertical = ClampDeadZone(Input.GetAxisRaw("Vertical"));
+                _R_horizontal = ClampDeadZone(Input.GetAxisRaw("Horizontal2"));
+                _R_vertical = ClampDeadZone(Input.GetAxisRaw("Vertical2"));
+            }
+            else
+            {
+                _L_horizontal = ClampDeadZone(Input.GetAxis("Horizontal"));
+                _L_vertical = ClampDeadZone(Input.GetAxis("Vertical"));
+                _R_horizontal = ClampDeadZone(Input.GetAxis("Horizontal2"));
+                _R_vertical = ClampDeadZone(Input.GetAxis("Vertical2"));
+            }
+        }
+
+        private Vector3 GetLeftStickVector()
+        {
+            return new Vector3(_L_horizontal, 0.0f, _L_vertical);
+        }
+        private Vector3 GetRightStickVector()
+        {
+            return new Vector3(_R_horizontal, 0.0f, _R_vertical);
+        }
+
+        private float ClampDeadZone(float value)
+        {
+            if (value <= _deadZone && value >= -_deadZone)
+            {
+                return 0.0f;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
